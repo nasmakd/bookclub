a87d91a4172126a13cff6e535c33a21ae9831976
nasmakd <ahffk1231@naver.com>
2022-12-18 16:37:37 +0900

    검색기능 추가

--- js/main.js
+++ js/main.js
@@ -1,473 +1,3 @@
-let bestMonth = [
-  {
-    img: 'img/new1.jpg',
-    title: '불편한 편의점 2',
-    autor: '김호연 저'
-  },
-  {
-    img: 'img/best (31).jpg',
-    title: '200조 이커머스 시대, 당신의 미래를 바꿀 21가지 이야기',
-    autor: '이종우,조재운 저'
-  },
-  {
-    img: 'img/best (1).png',
-    title: '혼자 있는 시간의 힘',
-    autor: '사이토 다카시 저/장은주 역'
-  },
-  {
-    img: 'img/best (22).jpg' ,
-    title: '불편한 편의점',
-    autor: '김호연 저'
-  },
-  {
-    img: 'img/best1.jpg',
-    title: '역행자',
-    autor: '자청 저'
-  },
-  {
-    img: 'img/best (26).jpg',
-    title: '월급쟁이로 시작한 38살 그녀는 어떻게 30억을 벌어 파이어족이 되었을까?',
-    autor: '원부연 저'
-  },
-  {
-    img: 'img/best (5).jpg',
-    title: '월급쟁이 재테크 상식사전(2022년 개정판)',
-    autor: '우용표 저'
-  },
-  {
-    img: 'img/best (24).jpg',
-    title: '강성태 66일 영어회화',
-    autor: '강성태 저'
-  },
-  {
-    img: 'img/best (8).jpg',
-    title: '내가 틀릴 수도 있습니다',
-    autor: '비욘 나티코 린데블라드 저/토마스 산체스 그림/박미경 역'
-  },
-  {
-    img: 'img/best (10).jpg',
-    title: '수상한 목욕탕',
-    autor: '마쓰오 유미 저/이수은 역'
-  },
-  {
-    img: 'img/best (12).jpg',
-    title: '당신만 모르는 일의 법칙 51',
-    autor: '이혜운'
-  },
-  {
-    img: 'img/best (11).jpg',
-    title: '류',
-    autor: '이천 저'
-  },
-  {
-    img: 'img/best (14).jpg',
-    title: '진짜 게으른 사람이 쓴 게으름 탈출법',
-    autor: '지이 저'
-  },
-  {
-    img: 'img/best (3).jpg',
-    title: '나는 노후에 가난하지 않기로 결심했다',
-    autor: '서대리 저'
-  },
-  {
-    img: 'img/best (4).jpg',
-    title: '경제 상식사전',
-    autor: '김민구 저'
-  },
-  {
-    img: 'img/best (6).jpg',
-    title: '우주시대에 오신 것을 환영합니다',
-    autor: '켈리 제라디 저/이지민 역'
-  },
-  {
-    img: 'img/best (7).jpg',
-    title: '클루지',
-    autor: '개리 마커스 저/최호영 역'
-  },
-];
-let bestWeek = [
-  {
-    img: 'img/new1.jpg',
-    title: '불편한 편의점 2',
-    autor: '김호연 저'
-  },
-  {
-    img: 'img/best (31).jpg',
-    title: '200조 이커머스 시대, 당신의 미래를 바꿀 21가지 이야기',
-    autor: '이종우,조재운 저'
-  },
-  {
-    img: 'img/best (1).png',
-    title: '혼자 있는 시간의 힘',
-    autor: '사이토 다카시 저/장은주 역'
-  },
-  {
-    img: 'img/best (19).jpg',
-    title: '이토록 뜻밖의 뇌과학',
-    autor: '리사 펠드먼 배럿 저/변지영 역/정재승 감수'
-  },
-  {
-    img: 'img/new2.jpg',
-    title: '참 괜찮은 태도',
-    autor: '박지현 저'
-  },
-  {
-    img: 'img/best (1).jpg',
-    title: '어떻게 원하는 것을 얻는가',
-    autor: '스튜어트 다이아몬드 저/김태훈 역'
-  },
-  {
-    img: 'img/best (22).jpg' ,
-    title: '불편한 편의점',
-    autor: '김호연 저'
-  },
-  {
-    img: 'img/best (15).jpg',
-    title: '당신은 일을 못하는 게 아니라 말을 못하는 겁니다',
-    autor: '이규희 저'
-  },
-  {
-    img: 'img/best1.jpg',
-    title: '역행자',
-    autor: '자청 저'
-  },
-  {
-    img: 'img/best (26).jpg',
-    title: '월급쟁이로 시작한 38살 그녀는 어떻게 30억을 벌어 파이어족이 되었을까?',
-    autor: '원부연 저'
-  },
-  {
-    img: 'img/best (16).jpg',
-    title: '나의 마지막 영어공부',
-    autor: '박소운 저'
-  },
-  {
-    img: 'img/best (17).jpg',
-    title: '주식 단타로 매일매일 벌어봤어?',
-    autor: '양선호 저'
-  },
-  {
-    img: 'img/best (5).jpg',
-    title: '월급쟁이 재테크 상식사전(2022년 개정판)',
-    autor: '우용표 저'
-  },
-  {
-    img: 'img/best (18).jpg',
-    title: '아직 긴 인생이 남았습니다',
-    autor: '기시미 이치로 저/전경아 역'
-  },
-  {
-    img: 'img/best (33).jpg',
-    title: '위닝',
-    autor: '팀 그로버,샤리 웽크 공저/이수경 역'
-  },
-  {
-    img: 'img/best (34).jpg',
-    title: '나의 빈틈을 채워주는 교양 콘서트',
-    autor: '김도균,이용주 저'
-  },
-  {
-    img: 'img/best (24).jpg',
-    title: '강성태 66일 영어회화',
-    autor: '강성태 저'
-  },
-  {
-    img: 'img/best (35).jpg',
-    title: '명쾌하고 야무진 최신 경제용어 해설',
-    autor: '권기대 저'
-  }
-];
-let bestDay = [
-  {
-    img: 'img/new1.jpg',
-    title: '불편한 편의점 2',
-    autor: '김호연 저'
-  },
-  {
-    img: 'img/best (28).jpg',
-    title: '킨포크 트래블 KINFOLK TRAVEL',
-    autor: '존 번스 저/김선희 역'
-  },
-  {
-    img: 'img/best (29).jpg',
-    title: '그래서 우리는 달에 간다',
-    autor: '곽재식 저'
-  },
-  {
-    img: 'img/best (30).jpg',
-    title: '죽을 때까지 책읽기',
-    autor: '니와 우이치로 저/이영미 역'
-  },
-  {
-    img: 'img/best (31).jpg',
-    title: '200조 이커머스 시대, 당신의 미래를 바꿀 21가지 이야기',
-    autor: '이종우,조재운 저'
-  },
-  {
-    img: 'img/best (1).png',
-    title: '혼자 있는 시간의 힘',
-    autor: '사이토 다카시 저/장은주 역'
-  },
-  {
-    img: 'img/best (19).jpg',
-    title: '이토록 뜻밖의 뇌과학',
-    autor: '리사 펠드먼 배럿 저/변지영 역/정재승 감수'
-  },
-  {
-    img: 'img/new2.jpg',
-    title: '참 괜찮은 태도',
-    autor: '박지현 저'
-  },
-  {
-    img: 'img/best (1).jpg',
-    title: '어떻게 원하는 것을 얻는가',
-    autor: '스튜어트 다이아몬드 저/김태훈 역'
-  },
-  {
-    img: 'img/best (20).jpg',
-    title: '나를 돌파하는 힘',
-    autor: '윤석금 저'
-  },
-  {
-    img: 'img/best (22).jpg' ,
-    title: '불편한 편의점',
-    autor: '김호연 저'
-  },
-  {
-    img: 'img/best (21).jpg',
-    title: '세계사 만물관',
-    autor: '피에르 싱가라벨루,실뱅 브네르 공저/김아애 역'
-  },
-  {
-    img: 'img/best (23).jpg',
-    title: '다이어트 레볼루션',
-    autor: '찰스 아이젠스타인 저/이보미 역'
-  },
-  {
-    img: 'img/best (24).jpg',
-    title: '오십에 읽는 노자 도덕경',
-    autor: '김옥림 저'
-  },
-  {
-    img: 'img/best (15).jpg',
-    title: '당신은 일을 못하는 게 아니라 말을 못하는 겁니다',
-    autor: '이규희 저'
-  },
-  {
-    img: 'img/best1.jpg',
-    title: '역행자',
-    autor: '자청 저'
-  },
-  {
-    img: 'img/best (26).jpg',
-    title: '월급쟁이로 시작한 38살 그녀는 어떻게 30억을 벌어 파이어족이 되었을까?',
-    autor: '원부연 저'
-  },
-  {
-    img: 'img/best (27).jpg',
-    title: '삶이 허기질 때 나는 교양을 읽는다',
-    autor: '지식 브런치 저'
-  },
-  {
-    img: 'img/best (8).jpg',
-    title: '내가 틀릴 수도 있습니다',
-    autor: '비욘 나티코 린데블라드 저/토마스 산체스 그림/박미경 역'
-  },
-  {
-    img: 'img/best (9).jpg',
-    title: '가재가 노래하는 곳',
-    autor: '델리아 오언스 저/김선형 역'
-  },
-  {
-    img: 'img/best (10).jpg',
-    title: '수상한 목욕탕',
-    autor: '마쓰오 유미 저/이수은 역'
-  },
-];
-
-
-let newBook = [
-  {
-    img: 'img/new1.jpg',
-    title: '불편한 편의점 2',
-    autor: '김호연 저'
-  },
-  {
-    img: 'img/new2.jpg',
-    title: '참 괜찮은 태도',
-    autor: '박지현 저'
-  },
-  {
-    img: 'img/new3.jpg',
-    title: '한국어 동사 사전',
-    autor: '김현권,송철의,박만규,권재일 저'
-  },
-  {
-    img: 'img/new4.jpg',
-    title: '살면서 꼭 한 번은 군주론',
-    autor: '니콜로 마키아벨리 저/최윤아 역'
-  },
-  {
-    img: 'img/new5.jpg',
-    title: '참장',
-    autor: '김형찬 저'
-  },
-  {
-    img: 'img/new6.jpg',
-    title: '파도를 넘어서 케이크',
-    autor: '이재연 저'
-  },
-  {
-    img: 'img/new7.jpg',
-    title: '면접을 위한 CS 전공지식 노트',
-    autor: '주홍철 저'
-  },
-  {
-    img: 'img/new8.jpg',
-    title: '음식패설',
-    autor: '김정희 저'
-  },
-];
-let choice = [
-  {
-    cmt: '제68회 현대문학상 수상작가 안보윤 장편소설',
-    img: 'img/choice1.jpg',
-    title: '밤의 행방 - 새소설 03',
-    autor: '안보윤 저'
-  },
-  {
-    cmt: '더 이상 설명이 필요 없는 20세기 환경학 최고의 고전',
-    img: 'img/choice2.jpg',
-    title: '침묵의 봄',
-    autor: '레이첼 카슨 저/김은령 역/홍욱희 감수'
-  },
-  {
-    cmt: '한 남자를 둘러싼 두 여성의 아슬아슬한 심리게임',
-    img: 'img/choice3.jpg',
-    title: '당신이 내게 최면을 걸었나요?',
-    autor: '리안 모리아티 저/김소정 역'
-  },
-  {
-    cmt: '느리지만 선명하게, 달라지는 나를 만나는 길',
-    img: 'img/choice4.jpg',
-    title: '피아니스트는 아니지만 매일 피아노를 칩니다',
-    autor: '김여진 저'
-  },
-  {
-    cmt: '낯선 도시가 들려주는 이야기. 문명, 인간, 나와의 만남, 유럽도시기행',
-    img: 'img/choice5.jpg',
-    title: '유럽 도시 기행 2',
-    autor: '유시민 저'
-  },
-  {
-    cmt: '마음은 우리가 어디서 왔는지 알려줄 거야',
-    img: 'img/choice6.jpg',
-    title: '당신의 손길이 닿기 전에',
-    autor: '리사 윈게이트 저/박지선 역'
-  },
-  {
-    cmt: '감동적이고 슬프고, 너무나 아름다운',
-    img: 'img/choice7.jpg',
-    title: '숨결이 바람 될 때',
-    autor: '폴 칼라니티 저/이종인 역'
-  }
-]
-let sel1 = [
-  {
-    img: 'img/sel11.jpg',
-    title: '오십에 읽는 논어',
-    autor: '최종엽 저'
-  },
-  {
-    img: 'img/sel12.jpg',
-    title: '필로소피 랩',
-    autor: '조니 톰슨 저/최다인 역'
-  },
-  {
-    img: 'img/sel13.jpg',
-    title: '소피의 세계',
-    autor: '요슈타인 가아더 저/장영은 역'
-  },
-  {
-    img: 'img/sel14.jpg',
-    title: '사는 게 힘드냐고 니체가 물었다',
-    autor: '박찬국 저'
-  },
-  {
-    img: 'img/sel15.jpg',
-    title: '행복의 조건',
-    autor: '조지 베일런트 저/이덕남 역/이시형 감수 저'
-  },
-  {
-    img: 'img/sel16.jpg',
-    title: '니체와 함께 산책을',
-    autor: '시라토리 하루히코 저/김윤경 역'
-  },
-]
-let sel2 = [
-  {
-    img: 'img/sel21.jpg',
-    title: '꽃을 사는 여자들',
-    autor: '바네사 몽포르 저/서경홍 역'
-  },
-  {
-    img: 'img/sel22.jpg',
-    title: '여기, 우리가 만나는 곳',
-    autor: '존 버거 저/강수정 역'
-  },
-  {
-    img: 'img/sel23.jpg',
-    title: '빅 피쉬',
-    autor: '대니얼 월리스 저/장영희 역'
-  },
-  {
-    img: 'img/sel24.jpg',
-    title: '오후도 서점 이야기',
-    autor: '무라야마 사키 저/류순미 역'
-  },
-  {
-    img: 'img/sel25.png',
-    title: '버드나무에 부는 바람',
-    autor: '케네스 그레이엄 저/안미란 역'
-  },
-  {
-    img: 'img/sel26.jpg',
-    title: '가끔 너를 생각해',
-    autor: '후지마루 저/김수지 역'
-  },
-]
-let sel3 = [
-  {
-    img: 'img/sel31.jpg',
-    title: '요즘 사는 맛',
-    autor: '김겨울,김현민,김혼비,디에디트,박서련,박정민,손현,요조,임진아,천선란,최민석,핫펠트 저'
-  },
-  {
-    img: 'img/sel32.jpg',
-    title: '처음 읽는 음식의 세계사',
-    autor: '미야자키 마사카츠 저/한세희 역'
-  },
-  {
-    img: 'img/sel33.jpg',
-    title: '세상에 맛있는 와인이 너무 많아서',
-    autor: '와인디렉터 양갱 저'
-  },
-  {
-    img: 'img/sel34.jpg',
-    title: '애플 타르트를 구워 갈까 해',
-    autor: '박지원 저'
-  },
-  {
-    img: 'img/sel35.jpg',
-    title: '아무튼, 술집',
-    autor: '김혜경 저'
-  },
-  {
-    img: 'img/sel36.jpg',
-    title: '음식 고전',
-    autor: '한복려,한복진,이소영 공저'
-  },
-]
 
 bestMonth.map((i)=>{
   let bestCont = `
@@ -476,7 +6,7 @@ bestMonth.map((i)=>{
       <div class="image_box"><img src='${i.img}' alt='${i.title}' /></div>
       <div class="txt_box">
         <p class='book_title'>${i.title}</p>
-        <p class='autor'>${i.autor}</p>
+        <p class='author'>${i.author}</p>
       </div>
     </a>
   </li>
@@ -490,7 +20,7 @@ bestWeek.map((i)=>{
       <div class="image_box"><img src='${i.img}' alt='${i.title}' /></div>
       <div class="txt_box">
         <p class='book_title'>${i.title}</p>
-        <p class='autor'>${i.autor}</p>
+        <p class='author'>${i.author}</p>
       </div>
     </a>
   </li>
@@ -504,7 +34,7 @@ bestDay.map((i)=>{
       <div class="image_box"><img src='${i.img}' alt='${i.title}' /></div>
       <div class="txt_box">
         <p class='book_title'>${i.title}</p>
-        <p class='autor'>${i.autor}</p>
+        <p class='author'>${i.author}</p>
       </div>
     </a>
   </li>
@@ -522,7 +52,7 @@ newBook.map((i)=>{
         </div>
         <div class="txt_box">
           <p class="book_title">${i.title}</p>
-          <p class="autor">${i.autor}</p>
+          <p class="author">${i.author}</p>
         </div>
       </a>
     </li>
@@ -540,7 +70,7 @@ choice.map((i)=>{
       <div class="center_txt_box ch_txt_box">
         <p class="cmt">${i.cmt}</p>
         <p class="book_title">${i.title}</p>
-        <p class="autor">${i.autor}</p>
+        <p class="author">${i.author}</p>
       </div>
     </a>
   </li>
@@ -558,7 +88,7 @@ sel1.map((i)=>{
       </div>
       <div class="center_txt_box sel_txt_box">
         <p class="book_title">${i.title}</p>
-        <p class="autor">${i.autor}</p>
+        <p class="author">${i.author}</p>
       </div>
     </a>
   </li>
@@ -575,7 +105,7 @@ sel2.map((i)=>{
       </div>
       <div class="center_txt_box sel_txt_box">
         <p class="book_title">${i.title}</p>
-        <p class="autor">${i.autor}</p>
+        <p class="author">${i.author}</p>
       </div>
     </a>
   </li>
@@ -592,7 +122,7 @@ sel3.map((i)=>{
       </div>
       <div class="center_txt_box sel_txt_box">
         <p class="book_title">${i.title}</p>
-        <p class="autor">${i.autor}</p>
+        <p class="author">${i.author}</p>
       </div>
     </a>
   </li>
--- js/sub.js
+++ js/sub.js
@@ -1,26 +1,67 @@
-const moreBtns = document.querySelectorAll('.moreBtn');
-console.log(moreBtns);
-
+const moreBtns = document.querySelectorAll(".moreBtn");
 for (let i = 0; i < moreBtns.length; i++) {
-	moreBtns[i].addEventListener('click', (e) => {
-		const contains = e.target.parentNode.classList.contains('show');
-		if (contains) {
-			e.target.parentNode.classList.remove('show');
-			e.target.innerHTML = '펼치기 <img src="img/downArrow.png" alt="화살표">';
-			e.target.classList.remove('on');
-		} else {
-			e.target.parentNode.classList.add('show');
-			e.target.innerHTML = '접기 <img src="img/downArrow.png" alt="화살표">';
-			e.target.classList.add('on');
-		}
-	});
+  moreBtns[i].addEventListener("click", (e) => {
+    const contains = e.target.parentNode.classList.contains("show");
+    if (contains) {
+      e.target.parentNode.classList.remove("show");
+      e.target.innerHTML = '펼치기 <img src="img/downArrow.png" alt="화살표">';
+      e.target.classList.remove("on");
+    } else {
+      e.target.parentNode.classList.add("show");
+      e.target.innerHTML = '접기 <img src="img/downArrow.png" alt="화살표">';
+      e.target.classList.add("on");
+    }
+  });
 }
 
-const infoSlide = new Swiper('.info_slide_box', {
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  slidesPerView: 1,
-  spaceBetween: 35, 
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.querySelector(".searchInput");
+const filteredList = document.querySelector(".searchResults");
+const search_content = document.querySelector(".search_content");
+const results_length = document.querySelector(".results_length");
+const searchArr = [
+  ...bestMonth,
+  ...bestWeek,
+  ...bestDay,
+  ...newBook,
+  ...sel1,
+  ...sel2,
+  ...sel3,
+];
+const setSearchArr = [...new Set(searchArr.map(JSON.stringify))].map(
+  JSON.parse
+);
+
+const searchFunc = (obj) => {
+  search = searchInput.value;
+  return obj.indexOf(search) !== -1;
+};
+const showFilteredCont = (cont) => {
+  const filteredOne = document.createElement("li");
+  filteredOne.innerHTML = `
+		<a href="book_detail.html">
+			<div class="search-container">
+				<img src='${cont.img}' alt='${cont.title}'/>
+				<p class="title">${cont.title}</p>
+				<p class="author">${cont.author}</p>
+			</div>
+		</a>
+		`;
+  filteredList.append(filteredOne);
+};
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  filteredList.innerHTML = "";
+
+  if (searchInput.value) {
+    const filteredCont = setSearchArr.filter((x) => searchFunc(x.title));
+    results_length.innerText = filteredCont.length;
+    if (filteredCont) {
+      filteredCont.forEach((acc) => showFilteredCont(acc));
+    }
+    if (filteredCont.length == 0) {
+      filteredList.innerHTML = `<li class='result_none'>검색 결과가 존재하지 않습니다.</li>`;
+    }
+  }
 });
